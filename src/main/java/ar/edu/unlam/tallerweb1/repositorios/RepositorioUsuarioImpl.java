8832cb837e906de3c01b94cfc9758e76796f950b
LuisBlotta <luis.blotta@mdevz.com>
2020-05-12 20:50:57 -0300

    Publicar get

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioUsuarioImpl.java
@@ -15,8 +15,8 @@ import javax.inject.Inject;
 @Repository("repositorioUsuario")
 public class RepositorioUsuarioImpl implements RepositorioUsuario {
 
-	// Como todo repositorio maneja acciones de persistencia, normalmente estará inyectado el session factory de hibernate
-	// el mismo está difinido en el archivo hibernateContext.xml
+	// Como todo repositorio maneja acciones de persistencia, normalmente estara inyectado el session factory de hibernate
+	// el mismo esta difinido en el archivo hibernateContext.xml
 	private SessionFactory sessionFactory;
 
     @Autowired
@@ -29,12 +29,11 @@ public class RepositorioUsuarioImpl implements RepositorioUsuario {
 
 		// Se obtiene la sesion asociada a la transaccion iniciada en el servicio que invoca a este metodo y se crea un criterio
 		// de busqueda de Usuario donde el email y password sean iguales a los del objeto recibido como parametro
-		// uniqueResult da error si se encuentran más de un resultado en la busqueda.
+		// uniqueResult da error si se encuentran mas de un resultado en la busqueda.
 		final Session session = sessionFactory.getCurrentSession();
 		return (Usuario) session.createCriteria(Usuario.class)
 				.add(Restrictions.eq("email", usuario.getEmail()))
 				.add(Restrictions.eq("password", usuario.getPassword()))
 				.uniqueResult();
 	}
-
 }
