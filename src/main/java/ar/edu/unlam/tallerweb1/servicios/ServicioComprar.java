5914d7ee52e20694f7028b274e00b565d2761ef0
gohanxl <Lacquaniti1961>
2020-05-12 21:02:00 -0300

    Add methods to ServicioComprar

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComprar.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioComprar.java
@@ -1,4 +1,8 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import ar.edu.unlam.tallerweb1.modelo.Libro;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
 public interface ServicioComprar {
+    void comprarLibro(Libro libro_id, Usuario usuario_id);
 }
