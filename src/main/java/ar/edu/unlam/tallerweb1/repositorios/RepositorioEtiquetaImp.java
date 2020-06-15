408cfd78eb1da0cce818f8a19d85a5d00ba692f6
LuisBlotta <luis.blotta@mdevz.com>
2020-06-15 19:57:11 -0300

    Mover lógica de repositorio a servicio

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioEtiquetaImp.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioEtiquetaImp.java
@@ -59,11 +59,11 @@ public class RepositorioEtiquetaImp implements RepositorioEtiqueta {
     }
 
     @Override
-    public List<Publicacion> recomendarPublicaciones(Usuario user) {
+    public List<Etiqueta> listarEtiquetasporUsuario(Usuario user) {
 
         final Session session = sessionFactory.getCurrentSession();
         Criteria criteria = session.createCriteria(Compra.class)
-        .setProjection(Projections.property("e.descripcion"))
+                .setProjection(Projections.property("e.descripcion"))
                 .createAlias("publicacion", "pu")
                 .createAlias("usuario", "u")
                 .createAlias("pu.etiquetas", "e")
@@ -71,36 +71,23 @@ public class RepositorioEtiquetaImp implements RepositorioEtiqueta {
                 .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
         List<Etiqueta> etiquetas = criteria.list();
 
-        if(etiquetas.size() == 0)
-            return null;
-
-        Set<Etiqueta> hashSet = new HashSet<Etiqueta>(etiquetas);
-        etiquetas.clear();
-        etiquetas.addAll(hashSet);
-
-        List<Compra> compras = repositorioUsuarioDao.getCompras(user);
-        List<Publicacion> publicacionesDeCompras = compras.stream().map(Compra::getPublicacion).collect(Collectors.toList());
-        List<Long> publicacionesIds = publicacionesDeCompras.stream().map(Publicacion::getId).collect(Collectors.toList());
-
-        Criteria result = session.createCriteria(Compra.class)
-                .setProjection(Projections.property("publicacion"))
-                .createAlias("publicacion", "pu")
-                .createAlias("usuario", "u")
-                .createAlias("pu.etiquetas", "e")
-                .createAlias("pu.puntaje", "punt")
-                .add(Restrictions.ne("usuario", user))
-                .add(Restrictions.not(Restrictions.in("id", publicacionesIds)))
-                .add(Restrictions.in("e.descripcion", etiquetas))
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-        List<Publicacion> publicaciones = result.list();
-
-        List<Publicacion> publicacionesPorPuntaje = new ArrayList<Publicacion>();
-
-        for (int i = 0; i < publicaciones.size(); i++){
-           double promedio = repositorioPuntajeDao.consultarPuntajePromedio(publicaciones.get(i));
-            if (promedio >= 3)
-                publicacionesPorPuntaje.add(publicaciones.get(i));
-        }
-        return publicacionesPorPuntaje;
+        return etiquetas;
     }
+
+        @Override
+        public List<Publicacion> publicacionesPorEtiquetas(Usuario user, List<Etiqueta> etiquetas, List<Long> publicacionesIds) {
+            final Session session = sessionFactory.getCurrentSession();
+            Criteria result = session.createCriteria(Compra.class)
+                    .setProjection(Projections.property("publicacion"))
+                    .createAlias("publicacion", "pu")
+                    .createAlias("usuario", "u")
+                    .createAlias("pu.etiquetas", "e")
+                    .createAlias("pu.puntaje", "punt")
+                    .add(Restrictions.ne("usuario", user))
+                    .add(Restrictions.not(Restrictions.in("id", publicacionesIds)))
+                    .add(Restrictions.in("e.descripcion", etiquetas))
+                    .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+            List<Publicacion> publicaciones = result.list();
+            return publicaciones;
+        }
 }
--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPublicacionImp.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPublicacionImp.java
@@ -1,16 +1,23 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import ar.edu.unlam.tallerweb1.modelo.Compra;
 import ar.edu.unlam.tallerweb1.modelo.Etiqueta;
 import ar.edu.unlam.tallerweb1.modelo.Publicacion;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioEtiqueta;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioPublicacion;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioPuntaje;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service("servicioBuscarLibro")
 @Transactional
@@ -18,11 +25,17 @@ public class ServicioPublicacionImp implements ServicioPublicacion {
 
     private RepositorioPublicacion servicioPublicacionDao;
     private RepositorioEtiqueta servicioEtiquetaDao;
+    private RepositorioUsuario repositorioUsuarioDao;
+    private RepositorioPuntaje repositorioPuntajeDao;
+
+
 
     @Autowired
-    public ServicioPublicacionImp(RepositorioPublicacion servicioPublicacionDao, RepositorioEtiqueta servicioEtiquetaDao){
+    public ServicioPublicacionImp(RepositorioPublicacion servicioPublicacionDao, RepositorioEtiqueta servicioEtiquetaDao, RepositorioUsuario repositorioUsuarioDao, RepositorioPuntaje repositorioPuntajeDao){
         this.servicioPublicacionDao = servicioPublicacionDao;
         this.servicioEtiquetaDao = servicioEtiquetaDao;
+        this.repositorioUsuarioDao = repositorioUsuarioDao;
+        this.repositorioPuntajeDao = repositorioPuntajeDao;
     }
 
     @Override
@@ -49,9 +62,30 @@ public class ServicioPublicacionImp implements ServicioPublicacion {
 
     @Override
     public List<Publicacion> recomendarPublicaciones(Usuario user) {
-        return servicioEtiquetaDao.recomendarPublicaciones(user);
-    }
+        List<Etiqueta> etiquetas = servicioEtiquetaDao.listarEtiquetasporUsuario(user);
 
+        if (etiquetas.size() == 0)
+            return null;
+
+        Set<Etiqueta> hashSet = new HashSet<Etiqueta>(etiquetas);
+        etiquetas.clear();
+        etiquetas.addAll(hashSet);
+
+        List<Compra> compras = repositorioUsuarioDao.getCompras(user);
+        List<Publicacion> publicacionesDeCompras = compras.stream().map(Compra::getPublicacion).collect(Collectors.toList());
+        List<Long> publicacionesIds = publicacionesDeCompras.stream().map(Publicacion::getId).collect(Collectors.toList());
+
+        List<Publicacion> publicaciones = servicioEtiquetaDao.publicacionesPorEtiquetas(user, etiquetas, publicacionesIds);
+
+        List<Publicacion> publicacionesPorPuntaje = new ArrayList<Publicacion>();
+
+        for (int i = 0; i < publicaciones.size(); i++){
+            double promedio = repositorioPuntajeDao.consultarPuntajePromedio(publicaciones.get(i));
+            if (promedio >= 3)
+                publicacionesPorPuntaje.add(publicaciones.get(i));
+        }
+        return publicacionesPorPuntaje;
+        }
 
     @Override
     public List<Publicacion> listarPublicacionDeUsuario(Usuario usuario) {
