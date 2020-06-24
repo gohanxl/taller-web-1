a42785421b49fcd15ff53a421d901ba102db7b07
gohanxl <Lacquaniti1961>
2020-06-24 20:48:56 -0300

    Create folder for controller tests

---
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorBibliotecaTest.java
@@ -0,0 +1,77 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioUsuarioImpl;
+import ar.edu.unlam.tallerweb1.servicios.ServicioUsuarioImpl;
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+        "classpath:/applicationContext.xml"
+})
+public class ControladorBibliotecaTest {
+
+    private MockMvc mockMvc;
+    private MockHttpSession session;
+    private MockHttpServletRequest request;
+    private ControladorBiblioteca controladorBiblioteca;
+    private ServicioUsuarioImpl servicioUsuario;
+    private RepositorioUsuarioImpl repositorioUsuario;
+    private Usuario usuario = new Usuario();
+    private Gson gson = new Gson();
+    private String jsonString;
+
+    @Before
+    public void setUp() {
+
+        servicioUsuario = mock(ServicioUsuarioImpl.class);
+        repositorioUsuario = mock(RepositorioUsuarioImpl.class);
+        session = new MockHttpSession();
+        request = new MockHttpServletRequest();
+        controladorBiblioteca = new ControladorBiblioteca(servicioUsuario);
+        mockMvc = MockMvcBuilders.standaloneSetup((controladorBiblioteca)).build();
+
+        usuario.setId((long) 1);
+        usuario.setRol("admin");
+        usuario.setNombre("Lucas");
+        usuario.setPassword("1234");
+        usuario.setEmail("lucas@gmail.com");
+
+        jsonString = gson.toJson(usuario);
+    }
+
+    @Test
+    public void testQueChequeaQueSiHayUsuarioVayaABiblioteca() throws Exception {
+
+        session.setAttribute("USUARIO", usuario);
+
+        this.mockMvc.perform(get("/biblioteca").session(session)
+                .param("request", "mockedRequest")
+                .accept(MediaType.ALL))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void testQueChequeaQueSiHayUsuarioVayaARedirect() throws Exception {
+
+        this.mockMvc.perform(get("/biblioteca").session(session)
+                .param("request", "mockedRequest")
+                .accept(MediaType.ALL))
+                .andExpect(status().is3xxRedirection());
+    }
+}
