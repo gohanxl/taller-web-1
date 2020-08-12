7a1defce2e00d4cce7400c9da0e963bebada8708
Isaias <isaias.caporusso@gmail.com>
2020-08-12 19:42:12 -0300

    FINAL

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioEmailImp.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioEmailImp.java
@@ -201,7 +201,7 @@ public class ServicioEmailImp implements ServicioEmail {
 				"                                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;\">\n" +
 				"                                  <tbody>\n" +
 				"                                    <tr>\n" +
-				"                                      <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;\"> <a href=\"http://localhost:8080/historial-de-transacciones\" target=\"_blank\" style=\"display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;\">Ver ventas</a> </td>\n" +
+				"                                      <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;\"> <a href=\"http://localhost:8080/biblioteca\" target=\"_blank\" style=\"display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;\">Ver Libros</a> </td>\n" +
 				"                                    </tr>\n" +
 				"                                  </tbody>\n" +
 				"                                </table>\n" +
@@ -375,7 +375,7 @@ public class ServicioEmailImp implements ServicioEmail {
 				"                                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;\">\n" +
 				"                                  <tbody>\n" +
 				"                                    <tr>\n" +
-				"                                      <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;\"> <a href=\"http://localhost:8080/biblioteca\" target=\"_blank\" style=\"display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;\">Ver ventas</a> </td>\n" +
+				"                                      <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;\"> <a href=\"http://localhost:8080/mi-cuenta\" target=\"_blank\" style=\"display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;\">Ver ventas</a> </td>\n" +
 				"                                    </tr>\n" +
 				"                                  </tbody>\n" +
 				"                                </table>\n" +
